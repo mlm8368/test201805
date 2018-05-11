6e10af3e2b63802c91eed4febf06842da8668651
docGit <abc@nn.com>
2018-05-11 23:44:28 +0800

    s

--- src/modules/sxiaoyuan/index.ts
+++ src/modules/sxiaoyuan/index.ts
@@ -6,7 +6,6 @@ import '../../common/js/mui.init.js'
 import '../../mui/js/mui.back.5+.js'
 // self
 import './index/init.ts'
-import util from '../../common/js/util.js'
 import { $, plus, viewEXT, setImmersedHeight } from '../../common/js/global.js'
 // ready
 $.ready(function () {
@@ -16,12 +15,5 @@ $.ready(function () {
 })
 // plusReady
 $.plusReady(function () {
-  util.options.tabBarId = 'tabBarStudent'
-  util.options.subpages = ['../sxiaoyuan/index' + viewEXT, '../sbanji/index' + viewEXT, '../sbamaquan/index' + viewEXT, '../smore/index' + viewEXT]
-  util.initSubpage({})
-
-  let nview = plus.nativeObj.View.getViewById('tabBarStudent')
-  nview.addEventListener('click', function (e) {
-    $.clickTabBar(e)
-  })
+  $.noop()
 })
