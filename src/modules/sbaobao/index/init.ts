082f26784eb6636b5cd2fcb58fa3168bbd6ea3e3
docGit <abc@nn.com>
2018-05-13 03:15:38 +0800

    s

--- src/modules/sbaobao/index/init.ts
+++ src/modules/sbaobao/index/init.ts
@@ -1,9 +1,6 @@
 import { $, viewEXT } from '../../../common/js/global.js'
 // import util from '../../../common/js/util.js'
 
-let activePage = null
-
-$.plus = null
 $.init({
   gestureConfig: { tap: true, doubletap: false, longtap: false, hold: false, flick: false, swipe: false, drag: false, pinch: false }
 })
