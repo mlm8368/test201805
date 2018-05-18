d4de5e1d122e99fc1bc842c7d67fa17341222bb0
docGit <abc@nn.com>
2018-05-18 12:31:56 +0800

    s

--- src/mui/js/popovers.js
+++ src/mui/js/popovers.js
@@ -226,43 +226,6 @@
 		setStyle(popover, 'block', pTop, pLeft);
 	};
 
-	$.createMask = function(callback) {
-		var element = document.createElement('div');
-		element.classList.add(CLASS_BACKDROP);
-		element.addEventListener($.EVENT_MOVE, $.preventDefault);
-		element.addEventListener('tap', function() {
-			mask.close();
-		});
-		var mask = [element];
-		mask._show = false;
-		mask.show = function() {
-			mask._show = true;
-			element.setAttribute('style', 'opacity:1');
-			document.body.appendChild(element);
-			return mask;
-		};
-		mask._remove = function() {
-			if (mask._show) {
-				mask._show = false;
-				element.setAttribute('style', 'opacity:0');
-				$.later(function() {
-					var body = document.body;
-					element.parentNode === body && body.removeChild(element);
-				}, 350);
-			}
-			return mask;
-		};
-		mask.close = function() {
-			if (callback) {
-				if (callback() !== false) {
-					mask._remove();
-				}
-			} else {
-				mask._remove();
-			}
-		};
-		return mask;
-	};
 	$.fn.popover = function() {
 		var args = arguments;
 		this.each(function() {
