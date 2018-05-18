6f2e74872e9db1da8a4e7ce9799fbfe566ec6d12
docGit <abc@nn.com>
2018-05-18 10:45:42 +0800

    s

--- src/common/js/mui.init.js
+++ src/common/js/mui.init.js
@@ -47,6 +47,43 @@ import 'mui/js/mui.dialog.toast';
   $.targets = {};
   $.targets._popover = null;
   $.closePopup = $.noop;
+  $.createMask = function(callback) {
+		var element = document.createElement('div');
+		element.classList.add(CLASS_BACKDROP);
+		element.addEventListener($.EVENT_MOVE, $.preventDefault);
+		element.addEventListener('tap', function() {
+			mask.close();
+		});
+		var mask = [element];
+		mask._show = false;
+		mask.show = function() {
+			mask._show = true;
+			element.setAttribute('style', 'opacity:1');
+			document.body.appendChild(element);
+			return mask;
+		};
+		mask._remove = function() {
+			if (mask._show) {
+				mask._show = false;
+				element.setAttribute('style', 'opacity:0');
+				$.later(function() {
+					var body = document.body;
+					element.parentNode === body && body.removeChild(element);
+				}, 350);
+			}
+			return mask;
+		};
+		mask.close = function() {
+			if (callback) {
+				if (callback() !== false) {
+					mask._remove();
+				}
+			} else {
+				mask._remove();
+			}
+		};
+		return mask;
+	};
   // 扩展
   $.log = function(str) {
     let myarraylist = str;
