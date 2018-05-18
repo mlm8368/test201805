a7505badeb53725b799a072c75c6d902f89aa06f
mlm8368 <ym001@126.com>
2018-05-18 08:56:48 +0800

    s

--- src/modules/sbaobao/index/config.ts
+++ src/modules/sbaobao/index/config.ts
@@ -2,10 +2,10 @@ import { viewEXT } from '../../../common/js/global.js'
 
 export * from '../../../common/js/config.js'
 
-const subpageStyleXiaoyuan = { top: '0px', titleNView: { backgroundColor: '#FFff00', titleText: '校园', titleColor: '#CCCCCC' } }
-const subpageStyleBanji = { top: '0px', titleNView: { backgroundColor: '#540000', titleText: '班级', titleColor: '#CCCCCC' } }
-const subpageStyleBamaquan = { top: '0px', titleNView: { backgroundColor: '#668800', titleText: '爸妈圈', titleColor: '#CCCCCC' } }
-const subpageStyleMore = { top: '0px', titleNView: { backgroundColor: '#aacc00', titleText: '更多', titleColor: '#CCCCCC' } }
+const subpageStyleXiaoyuan = { top: '0px', backButtonAutoControl: 'none', titleNView: { backgroundColor: '#FFff00', titleText: '校园', titleColor: '#CCCCCC' } }
+const subpageStyleBanji = { top: '0px', backButtonAutoControl: 'none', titleNView: { backgroundColor: '#540000', titleText: '班级', titleColor: '#CCCCCC' } }
+const subpageStyleBamaquan = { top: '0px', backButtonAutoControl: 'none', titleNView: { backgroundColor: '#668800', titleText: '爸妈圈', titleColor: '#CCCCCC' } }
+const subpageStyleMore = { top: '0px', backButtonAutoControl: 'none', titleNView: { backgroundColor: '#aacc00', titleText: '更多', titleColor: '#CCCCCC' } }
 export const footbarProp = {
   tabBarId: 'tabBarStudent',
   firstWebviewId: 'sbaobao_index',
