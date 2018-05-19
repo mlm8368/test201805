661a2fba42a09b65ecd0606a422dc7dc8ee05d06
docGit <abc@nn.com>
2018-05-19 23:32:11 +0800

    s

--- src/modules/sbaobao/baobao.ts
+++ src/modules/sbaobao/baobao.ts
@@ -4,12 +4,12 @@ import '../../common/sass/mui.init.scss'
 // mui js
 import '../../common/js/mui.init.js'
 // self
-import { $, viewEXT, setImmersedHeight } from '../../common/js/global.js'
+import { $, viewEXT } from '../../common/js/global.js'
 // ready
 $.init({
   swipeBack: false,
   keyEventBind: { backbutton: false, menubutton: false },
-  gestureConfig: { tap: true, swipe: true, doubletap: false, longtap: false, hold: false, flick: false, drag: false, pinch: false }
+  gestureConfig: { tap: true, swipe: false, doubletap: false, longtap: false, hold: false, flick: false, drag: false, pinch: false }
 })
 $.ready(function () {
   if (viewEXT === '.htm') return
