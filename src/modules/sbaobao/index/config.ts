23a18bdf0ab6b475804bafca165f126e4657da3f
docGit <abc@nn.com>
2018-05-20 22:55:42 +0800

    s

--- src/modules/sbaobao/index/config.ts
+++ src/modules/sbaobao/index/config.ts
@@ -3,7 +3,7 @@ import { viewEXT } from '../../../common/js/global.js'
 export * from '../../../common/js/config.js'
 
 const subpageStyleXiaoyuan = { top: '0px', backButtonAutoControl: 'none', titleNView: { backgroundColor: '#FFff00', titleText: '阳光美智幼儿园', titleColor: '#CCCCCC' },
-  subNViews: [{ id: 'schoolSlider', type: 'ImageSlider', styles: { left: '0px', right: '0px', top: '0px', height: '200px', position: 'static', loop: true, images: [] } }] }
+  subNViews: [{ id: 'schoolSlider', type: 'ImageSlider', styles: { left: '0px', right: '0px', top: '0px', height: '200px', position: 'static', autoplay: true, loop: true, images: [] } }] }
 const subpageStyleBanji = { top: '0px', backButtonAutoControl: 'none', titleNView: { backgroundColor: '#540000', titleText: '启蒙一班', titleColor: '#CCCCCC' } }
 const subpageStyleBamaquan = { top: '0px', backButtonAutoControl: 'none', titleNView: { backgroundColor: '#668800', titleText: '爸妈圈', titleColor: '#CCCCCC' } }
 const subpageStyleMore = { top: '0px', backButtonAutoControl: 'none', titleNView: { backgroundColor: '#aacc00', titleText: '更多', titleColor: '#CCCCCC' } }
