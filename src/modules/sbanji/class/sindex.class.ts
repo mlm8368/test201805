ff20d7cc59221e5923cf2743fc59bc6dc20b8b2c
docGit <abc@nn.com>
2018-06-02 17:41:46 +0800

    s

--- src/modules/sbanji/class/sindex.class.ts
+++ src/modules/sbanji/class/sindex.class.ts
@@ -1,14 +1,9 @@
 import { $, viewEXT } from '../../../common/js/global.js'
 import Student from '../../../class/student.class'
-import WebviewGroup from '../../../plugin/aui/webviewGroup.js'
-import { TabBarItem } from '../../../class/interface'
 import * as config from '../index/config'
 
 export default class SIndex extends Student {
   private main: any
-  // WebviewGroup
-  private group: any
-  private scrollTop = 0 // 显示卡片时上面部分完全卷入的高度
 
   constructor () {
     super()
