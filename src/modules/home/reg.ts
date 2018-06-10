9ae0ef588112f44e2fe2fc4cc12917e04b3ebc90
docGit <abc@nn.com>
2018-06-10 23:30:01 +0800

    s

---
+++ src/modules/home/reg.ts
@@ -0,0 +1,19 @@
+// css
+import '../../common/sass/mui.init.scss'
+import './reg/style.scss'
+// mui js
+import '../../common/js/mui.init.js'
+// self
+import { $, viewEXT } from '../../common/js/global.js'
+import Login from './class/login.class'
+
+let login = new Login()
+// ready
+$.ready(function () {
+  if (viewEXT === '.htm') return
+  $.noop()
+})
+// plusReady
+$.plusReady(function () {
+  $.noop()
+})
---
+++ src/modules/home/reg/config.ts
@@ -0,0 +1,162 @@
+export * from '../../../class/config'
+
+export const barTagsStudent = [
+  {
+    'tag': 'font',
+    'id': 'indexIcon',
+    'text': '\ue500',
+    'position': {
+      'top': '4px',
+      'left': '0',
+      'width': '20%',
+      'height': '24px'
+    },
+    'textStyles': {
+      'fontSrc': '_www/fonts/mui.ttf',
+      'align': 'center',
+      'size': '24px'
+    }
+  }, {
+    'tag': 'font',
+    'id': 'indexText',
+    'text': '宝宝',
+    'position': {
+      'top': '23px',
+      'left': '0',
+      'width': '20%',
+      'height': '24px'
+    },
+    'textStyles': {
+      'align': 'center',
+      'size': '10px'
+    }
+  }, {
+    'tag': 'font',
+    'id': 'newsIcon',
+    'text': '\ue300',
+    'position': {
+      'top': '4px',
+      'left': '20%',
+      'width': '20%',
+      'height': '24px'
+    },
+    'textStyles': {
+      'fontSrc': '_www/fonts/mui.ttf',
+      'align': 'center',
+      'size': '24px'
+    }
+  }, {
+    'tag': 'font',
+    'id': 'newsText',
+    'text': '学校',
+    'position': {
+      'top': '23px',
+      'left': '20%',
+      'width': '20%',
+      'height': '24px'
+    },
+    'textStyles': {
+      'align': 'center',
+      'size': '10px'
+    }
+  }, {
+    'tag': 'font',
+    'id': 'todoIcon',
+    'text': '\ue563',
+    'position': {
+      'top': '4px',
+      'left': '40%',
+      'width': '20%',
+      'height': '24px'
+    },
+    'textStyles': {
+      'fontSrc': '_www/fonts/mui.ttf',
+      'align': 'center',
+      'size': '24px'
+    }
+  }, {
+    'tag': 'font',
+    'id': 'todoText',
+    'text': '班级',
+    'position': {
+      'top': '24px',
+      'left': '40%',
+      'width': '20%',
+      'height': '24px'
+    },
+    'textStyles': {
+      'align': 'center',
+      'size': '10px'
+    }
+  }, {
+    'tag': 'font',
+    'id': 'contactIcon',
+    'text': '\ue203',
+    'position': {
+      'top': '4px',
+      'left': '60%',
+      'width': '20%',
+      'height': '24px'
+    },
+    'textStyles': {
+      'fontSrc': '_www/fonts/mui.ttf',
+      'align': 'center',
+      'size': '24px'
+    }
+  }, {
+    'tag': 'font',
+    'id': 'contactText',
+    'text': '爸妈圈',
+    'position': {
+      'top': '24px',
+      'left': '60%',
+      'width': '20%',
+      'height': '24px'
+    },
+    'textStyles': {
+      'align': 'center',
+      'size': '10px'
+    }
+  }, {
+    'tag': 'font',
+    'id': 'newwindowIcon',
+    'text': '\ue101',
+    'position': {
+      'top': '4px',
+      'left': '80%',
+      'width': '20%',
+      'height': '24px'
+    },
+    'textStyles': {
+      'fontSrc': '_www/fonts/mui.ttf',
+      'align': 'center',
+      'size': '24px'
+    }
+  }, {
+    'tag': 'font',
+    'id': 'newwindowText',
+    'text': '更多',
+    'position': {
+      'top': '24px',
+      'left': '80%',
+      'width': '20%',
+      'height': '24px'
+    },
+    'textStyles': {
+      'align': 'center',
+      'size': '10px'
+    }
+  }, {
+    'tag': 'rect',
+    'id': 'tabBorder',
+    'position': {
+      'top': '0',
+      'left': '0',
+      'width': '100%',
+      'height': '1px'
+    },
+    'rectStyles': {
+      'color': '#ccc'
+    }
+  }
+]
