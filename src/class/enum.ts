51b91a47f86de29aa763b0193ea386b73d1716d6
docGit <abc@nn.com>
2018-07-01 21:31:10 +0800

    s

--- src/class/enum.ts
+++ src/class/enum.ts
@@ -1,3 +1,8 @@
 export const enum appCacheKey {
-  sbaobao_school_classes = 200
+  home = 100,
+  sbaobao = 200,
+  sbaobao_school_classes = 201,
+  sxiaoyuan = 300,
+  sbanji = 400,
+  sbamaquan = 500
 }
