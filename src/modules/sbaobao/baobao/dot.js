4c40e890a7c3dc05765644f2195cae98b57420fc
docGit <abc@nn.com>
2018-07-03 14:26:50 +0800

    s

---
+++ src/modules/sbaobao/baobao/dot.js
@@ -0,0 +1,8 @@
+import tplBaobao from './tplBaobao.dot';
+export let dotBaobao = tplBaobao;
+
+import tplParent from './tplParent.dot';
+export let dotParent = tplParent;
+
+import tplSchool from './tplSchool.dot';
+export let dotSchool = tplSchool;
--- src/modules/sbaobao/class/sbaobao.class.ts
+++ src/modules/sbaobao/class/sbaobao.class.ts
@@ -3,6 +3,7 @@ import * as config from '../school/config'
 import Student from '../../../class/student.class'
 import { appCacheKey } from '../../../class/enum'
 import Cache from '../../../class/cache.class'
+import { dotBaobao, dotParent, dotSchool } from '../baobao/dot.js'
 
 export default class SBaobao extends Student {
   /**
