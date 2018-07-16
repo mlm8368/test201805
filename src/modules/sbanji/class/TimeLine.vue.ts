5acd06fc395fd9d852999d0d8ed41991c48f0f27
docGit <abc@nn.com>
2018-07-16 14:37:44 +0800

    s

--- src/modules/sbanji/class/TimeLine.vue.ts
+++ src/modules/sbanji/class/TimeLine.vue.ts
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
 
+const AppProps = Vue.extend({
+  props: {
+    oneday: {}
+  }
+})
+
 @Component({
   template: require('../index/timeline.vue.html')
 })
-export default class TimeLine extends Vue {
+export default class TimeLine extends AppProps {
   helloTimes: number = 0
   sayHello () {
     this.helloTimes++
