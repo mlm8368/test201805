4e2fe8c248c75e6737ee6afc990ed8e2e4a23f6f
docGit <abc@nn.com>
2018-07-17 11:55:29 +0800

    s

--- src/modules/sbaobao/back/baobao/dot.js
+++
@@ -1,7 +0,0 @@
-import tplBaobao from './tplBaobao.dot';
-import tplParent from './tplParent.dot';
-import tplSchool from './tplSchool.dot';
-
-export let baobao = tplBaobao;
-export let parent = tplParent;
-export let school = tplSchool;
