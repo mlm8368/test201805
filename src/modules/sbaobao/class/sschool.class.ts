841e7c945bfef63c97c17458cd7886b67ddb02fd
docGit <abc@nn.com>
2018-07-22 17:22:21 +0800

    s

--- src/modules/sbaobao/class/sschool.class.ts
+++ src/modules/sbaobao/class/sschool.class.ts
@@ -52,7 +52,7 @@ export default class SSchool extends Student {
     $.fire($.plus.webview.getWebviewById('sbaobao_index'), 'updateSubNViews', { studentid: this.studentid, classesid: currentClassesid })
     $.fire($.plus.webview.getWebviewById('sxiaoyuan_index'), 'updateTitleNViewTitle', { studentid: this.studentid, classesid: currentClassesid })
     $.fire($.plus.webview.getWebviewById('sbanji_index'), 'updateTitleNViewTitle', { studentid: this.studentid, classesid: currentClassesid })
-    // $.fire($.plus.webview.getWebviewById('sbanji_index'), 'refreshVueData')
+    $.fire($.plus.webview.getWebviewById('sbanji_index'), 'refreshVueData')
   }
 
   private renderSchool (schoolList, classesList) {
