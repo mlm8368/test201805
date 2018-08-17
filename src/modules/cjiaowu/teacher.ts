0f25e3ad6b566928f3381788d90dc257d993f855
docGit <abc@nn.com>
2018-08-17 13:11:36 +0800

    s

---
+++ src/modules/cjiaowu/teacher.ts
@@ -0,0 +1,32 @@
+// css
+import '../../common/sass/mui.init.scss'
+// import './index/style.scss'
+// mui js
+import '../../common/js/mui.init.js'
+// self
+import { $, viewEXT } from '../../common/js/global.js'
+import SMore from './class/more.class'
+
+const sMore = new SMore()
+
+// ready
+$.init({
+  swipeBack: false,
+  keyEventBind: { backbutton: false, menubutton: false },
+  gestureConfig: { tap: true, swipe: false, doubletap: false, longtap: false, hold: false, flick: false, drag: false, pinch: false }
+})
+$.ready(function () {
+  if (viewEXT === '.htm') return
+  $.noop()
+})
+// plusReady
+$.plusReady(function () {
+  $.noop()
+})
+// tap
+$.byId('logout').addEventListener('tap', () => {
+  sMore.logout()
+  sMore.alert('期待你尽快回来哦', () => {
+    $.plus.webview.close('cxiaowu_index')
+  })
+})
